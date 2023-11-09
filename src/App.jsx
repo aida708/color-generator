250f8a36d187ab0692dca0b6aff7c0ba4b439425
Aida <aidasufaj00@gmail.com>
2023-11-09 12:33:47 +0100

    value.js library

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import ColorList from "./ColorList";
 import Form from "./Form";
+import Values from "values.js";
 
 const App = () => {
+  const [colors, setColors] = useState([new Values("#f15025").all(10)]);
   return (
     <main>
       <h2>Color Generator Starter</h2>

c9608e8276b3fda8b01e1c4e1e6993e0c4088cb8
Aida <aidasufaj00@gmail.com>
2023-11-09 15:39:45 +0100

    react toastify

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from "react";
 import ColorList from "./ColorList";
 import Form from "./Form";
 import Values from "values.js";
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
   const [colors, setColors] = useState([new Values("#f15025").all(10)]);
 
+  toast.error;
   return (
     <main>
       <h2>Color Generator Starter</h2>
       <Form />
       <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
     </main>
   );
 };

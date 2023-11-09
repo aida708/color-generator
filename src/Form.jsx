2321d61f095ebd7d4b9b06a3c1fafa090a00fd0f
Aida <aidasufaj00@gmail.com>
2023-11-09 12:22:24 +0100

    html color input

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 //set-up a color state value and a form with an 2 input fields(color,text)
 export default function Form() {
@@ -8,13 +8,18 @@ export default function Form() {
   const handleSubmit = (e) => {
     setColor(e.target.value);
   };
-  handleChange = () => {};
+  //   handleChange = () => {};
   return (
     // set up a form with an 2 input fields: color, text
     <section className="container">
       <h4>Color Generator</h4>
       <form className="color-form" onSubmit={handleSubmit}>
-        <label htmlFor="color">Color</label>
+        <input
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+
         <input
           type="text"
           value={color}
